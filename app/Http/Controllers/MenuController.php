4801751e88fa4fdba44f0c7d7271b8c653d2c6e7
unknown <roberts.turner123dn@gmail.com>
2023-11-06 09:40:11 +0700

    Version 1.3 - By Thai Duy Vu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
@@ -11,7 +12,8 @@ class MenuController extends Controller
      */
     public function index()
     {
-        return view('navbar.menu');
+        $products = Product::all();
+        return view('navbar.menu', ['products' => $products]);
     }
 
     /**
